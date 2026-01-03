042b7808f48c4899ed9da76965b603a811058a20
kacloyew <kar_yellow@hotmail.com>
2026-01-03 18:03:32 +0100

    organización de carpetas necesarias

--- app/src/main/java/com/example/pokeapi/MainActivity.kt
+++ app/src/main/java/com/example/pokeapi/MainActivity.kt
@@ -1,47 +1,9 @@
 package com.example.pokeapi
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.pokeapi.ui.theme.PokeAPiTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            PokeAPiTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+class MainActivity : AppCompatActivity() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PokeAPiTheme {
-        Greeting("Android")
-    }
+
 }
