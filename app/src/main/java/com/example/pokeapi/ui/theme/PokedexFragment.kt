944e7f25303a0c0208d350c49790a2631bdddcf5
kacloyew <kar_yellow@hotmail.com>
2026-01-05 20:59:09 +0100

    commit añadida la opción de eliminar deslizando hacia la izquierda

--- app/src/main/java/com/example/pokeapi/ui/theme/PokedexFragment.kt
+++ app/src/main/java/com/example/pokeapi/ui/theme/PokedexFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.pokeapi.R  // ← IMPORTANTE: agregar este import
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pokeapi.R
 import com.example.pokeapi.databinding.FragmentPokedexBinding
 import com.example.pokeapi.model.Pokemon
 import com.example.pokeapi.ui.adapter.PokemonAdapter
@@ -17,7 +19,7 @@ class PokedexFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var adapter: PokemonAdapter
 
-    private val pokemonList = listOf(
+    private val pokemonList = mutableListOf(
         Pokemon(1, "Pikachu", "Ratón eléctrico amarillo"),
         Pokemon(2, "Charizard", "Dragón de fuego volador"),
         Pokemon(3, "Bulbasaur", "Semilla dinosaurio"),
@@ -38,10 +40,12 @@ class PokedexFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setupRecyclerView()
+        setupSwipeToDelete()
     }
 
     private fun setupRecyclerView() {
         adapter = PokemonAdapter(pokemonList) { pokemon ->
+
             // Cuando se hace click en un Pokémon
             val detailsFragment = DetallesFragment()
 
@@ -54,6 +58,34 @@ class PokedexFragment : Fragment() {
 
         binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.recyclerView.adapter = adapter
+    }
+
+    private fun setupSwipeToDelete() {
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+            0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+                val position = viewHolder.adapterPosition
+
+                // Eliminar el Pokémon de la lista
+                pokemonList.removeAt(position)
+
+                // Notificar al adapter que se eliminó un item
+                adapter.notifyItemRemoved(position)
+
+            }
+        }).attachToRecyclerView(binding.recyclerView)
     }
 
     override fun onDestroyView() {
